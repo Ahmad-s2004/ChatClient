5f62665590aa0d5feaece2646d6ea8830bd33fb6
Ahmad-s2004 <shahidahmadamman@gmail.com>
2026-05-03 15:27:13 +0500

    feat: add ProtectedRoute and implement auth-based navigation in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
 import Setting from './pages/Setting';
 
 function App() {
+
+  const isAuthenticated = () =>{
+    const user = JSON.parse(localStorage.getItem('profile'))
+    return user?.token ? true:false
+  }
+
+  const PrivateRoute = ({children}) => {
+    return isAuthenticated() ? children : <Navigate to="/signin"/>
+  }
+
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/setting" element={<Setting />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+        <Route path="/setting" element={<PrivateRoute><Setting /></PrivateRoute>} />
+        <Route path="/signin" element={
+          isAuthenticated()? <Signin />: <Navigate to="/"/>
+        } />
+        <Route path="/signup" element={
+          isAuthenticated()? <Signup />: <Navigate to="/"/>
+        } />
       </Routes>
     </Router>
   );
@@ -19,3 +34,42 @@ function App() {
 
 
 export default App
+
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import Signin from './pages/Signin';
+// import Signup from './pages/Signup';
+// import Home from './pages/Home';
+// import Setting from './pages/Setting';
+
+// function App() {
+
+//   const isAuthenticated = () => {
+//     const user = JSON.parse(localStorage.getItem('profile'));
+//     return user?.token ? true : false;
+//   }
+
+//   // Spelling fix: childern -> children
+//   const PrivateRoute = ({ children }) => {
+//     return isAuthenticated() ? children : <Navigate to="/signin" replace />;
+//   }
+
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+//         <Route path="/setting" element={<PrivateRoute><Setting /></PrivateRoute>} />
+        
+//         {/* Yahan function ko call kiya isAuthenticated() */}
+//         <Route path="/signin" element={
+//           !isAuthenticated() ? <Signin /> : <Navigate to="/" replace />
+//         } />
+        
+//         <Route path="/signup" element={
+//           !isAuthenticated() ? <Signup /> : <Navigate to="/" replace />
+//         } />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
